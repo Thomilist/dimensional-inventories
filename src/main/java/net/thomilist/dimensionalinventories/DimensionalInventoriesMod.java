fca8e098c5b4699afbe47e609170ba2d52190ac0
Thomas Emil Jensen <Thomas_98@hotmail.dk>
2023-05-04 11:53:22 +0200

    Fixed a crash in unconfigured dimensions
    - Crash happened when a player was present in an unconfigured dimension
    - Caused by uncaught exception when looking up the dimension pool
    - Exception throwing replaced with Optional
    - Repeat warnings in unconfigured dimensions reduced to avoid log spam
    - Reported in #3
    - Backport to 1.19.3 from 1.19.4

--- src/main/java/net/thomilist/dimensionalinventories/DimensionalInventoriesMod.java
+++ src/main/java/net/thomilist/dimensionalinventories/DimensionalInventoriesMod.java
@@ -8,8 +8,11 @@ import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.minecraft.entity.Entity;
 import net.minecraft.server.network.ServerPlayerEntity;
 
+import net.minecraft.world.GameMode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Optional;
 
 public class DimensionalInventoriesMod implements ModInitializer
 {
@@ -60,34 +63,38 @@ public class DimensionalInventoriesMod implements ModInitializer
 
 	public static void handlePlayerDimensionChange(ServerPlayerEntity player, String originDimensionName, String destinationDimensionName)
 	{
-		LOGGER.info("Player " + player.getName().getString() + " travelled from " + originDimensionName + " to " + destinationDimensionName + ".");
+		LOGGER.debug("Player " + player.getName().getString() + " travelled from " + originDimensionName + " to " + destinationDimensionName + ".");
 
 		if (DimensionPoolManager.samePoolContainsBoth(originDimensionName, destinationDimensionName))
 		{
-			LOGGER.info("The origin and destination dimensions are in the same pool. Player unaffected.");
+			LOGGER.debug("The origin and destination dimensions are in the same pool. Player unaffected.");
 		}
 		else
 		{
-			LOGGER.info("The origin and destination dimensions are in different pools. Switching inventories...");
+			LOGGER.debug("The origin and destination dimensions are in different pools. Switching inventories...");
 
-			String originDimensionPoolName;
-			String destinationDimensionPoolName;
+			Optional<DimensionPool> originDimension = DimensionPoolManager.getPoolWithDimension(originDimensionName);
+			Optional<DimensionPool> destinationDimension = DimensionPoolManager.getPoolWithDimension(destinationDimensionName);
 
-			try
+			if (originDimension.isEmpty() || destinationDimension.isEmpty())
 			{
-				originDimensionPoolName = DimensionPoolManager.getPoolWithDimension(originDimensionName).getName();
-				destinationDimensionPoolName = DimensionPoolManager.getPoolWithDimension(destinationDimensionName).getName();
-			}
-			catch (NullPointerException e)
-			{
-				LOGGER.warn("Not all dimensions are assigned a dimension pool. Player unaffected.");
+				LOGGER.warn("Not all dimensions are assigned a dimension pool. Player '" + player.getName().getString() + "' unaffected (" + originDimensionName + " -> " + destinationDimensionName + ").");
 				return;
 			}
+
+			String originDimensionPoolName = originDimension.get().getName();
+			String destinationDimensionPoolName = destinationDimension.get().getName();
 
 			InventoryManager.saveInventory(player, originDimensionPoolName);
 			InventoryManager.clearInventory(player);
 			InventoryManager.loadInventory(player, destinationDimensionPoolName);
-			player.changeGameMode(DimensionPoolManager.getGameModeOfDimensionPool(destinationDimensionPoolName));
+
+			Optional<GameMode> gameMode = DimensionPoolManager.getGameModeOfDimensionPool(destinationDimensionPoolName);
+			if (gameMode.isPresent())
+			{
+				player.changeGameMode(gameMode.get());
+			}
+
 		}
 
 		return;
@@ -97,17 +104,15 @@ public class DimensionalInventoriesMod implements ModInitializer
 	{
 		if (!DimensionPoolManager.samePoolContainsBoth(originDimensionName, destinationDimensionName))
 		{
-			LOGGER.info("Entity '" + newEntity.getName().getString() + "' travelled from " + originDimensionName + " to " + destinationDimensionName + ".");
-			LOGGER.info("The origin and destination dimensions are in different pools. Deleting entity...");
+			LOGGER.debug("Entity '" + newEntity.getName().getString() + "' travelled from " + originDimensionName + " to " + destinationDimensionName + ".");
+			LOGGER.debug("The origin and destination dimensions are in different pools. Deleting entity...");
 
-			try
+			Optional<DimensionPool> originDimension = DimensionPoolManager.getPoolWithDimension(originDimensionName);
+			Optional<DimensionPool> destinationDimension = DimensionPoolManager.getPoolWithDimension(destinationDimensionName);
+
+			if (originDimension.isEmpty() || destinationDimension.isEmpty())
 			{
-				DimensionPoolManager.getPoolWithDimension(originDimensionName).getName();
-				DimensionPoolManager.getPoolWithDimension(destinationDimensionName).getName();
-			}
-			catch (NullPointerException e)
-			{
-				LOGGER.warn("Not all dimensions are assigned a dimension pool. Entity unaffected.");
+				LOGGER.warn("Not all dimensions are assigned a dimension pool. Entity '" + newEntity.getName().getString() + "' unaffected (" + originDimensionName + " -> " + destinationDimensionName + ").");
 				return;
 			}
 
--- src/main/java/net/thomilist/dimensionalinventories/mixin/DisableAdvancementProgressMixin.java
+++ src/main/java/net/thomilist/dimensionalinventories/mixin/DisableAdvancementProgressMixin.java
@@ -1,7 +1,9 @@
 package net.thomilist.dimensionalinventories.mixin;
 
+import java.util.Optional;
 import java.util.function.Predicate;
 
+import net.thomilist.dimensionalinventories.LogThrottler;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -17,13 +19,15 @@ import net.thomilist.dimensionalinventories.DimensionPoolManager;
 @Mixin(AbstractCriterion.class)
 public abstract class DisableAdvancementProgressMixin<T extends AbstractCriterionConditions>
 implements Criterion<T> {
+    private static LogThrottler logThrottler = new LogThrottler(10000);
+
     @Inject(at = @At("HEAD"), method = "trigger(Lnet/minecraft/server/network/ServerPlayerEntity;Ljava/util/function/Predicate;)V", cancellable = true)
     public void trigger(ServerPlayerEntity player, Predicate<T> predicate, CallbackInfo info)
     {
         String dimensionName = player.getWorld().getRegistryKey().getValue().toString();
-        DimensionPool pool = DimensionPoolManager.getPoolWithDimension(dimensionName);
+        Optional<DimensionPool> pool = DimensionPoolManager.getPoolWithDimension(dimensionName, logThrottler.get());
 
-        if (!pool.canProgressAdvancements())
+        if (pool.isPresent() && !pool.get().canProgressAdvancements())
         {
             info.cancel();
         }
