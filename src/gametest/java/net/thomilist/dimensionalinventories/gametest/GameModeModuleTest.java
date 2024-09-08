cf7a22a3b0b51b8084d36429d6f215960a2a3880
Thomas Emil Jensen <66553086+Thomilist@users.noreply.github.com>
2024-09-08 20:26:14 +0200

    More logical test grouping; more tests
    - Grouped tests by modules
    - Added tests for game mode and status effect modules

---
+++ src/gametest/java/net/thomilist/dimensionalinventories/gametest/GameModeModuleTest.java
@@ -0,0 +1,56 @@
+package net.thomilist.dimensionalinventories.gametest;
+
+import net.fabricmc.fabric.api.entity.FakePlayer;
+import net.fabricmc.fabric.api.gametest.v1.FabricGameTest;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.test.GameTest;
+import net.minecraft.test.TestContext;
+import net.minecraft.world.GameMode;
+import net.thomilist.dimensionalinventories.gametest.util.BasicModSetup;
+
+public class GameModeModuleTest
+{
+    // When a player crosses dimension pools, their gamemode should be changed
+    // according to dimension pool settings
+    @GameTest(templateName = FabricGameTest.EMPTY_STRUCTURE)
+    public void transitionSwitchesGameMode(TestContext context)
+    {
+        var setup = new BasicModSetup();
+
+        // Using a Fabric API FakePlayer instead of a DummyServerPlayerEntity,
+        // since a server connection seems to be required to change game mode
+        var player = FakePlayer.get(context.getWorld());
+
+        var originPool = setup.dimensionPoolConfig.state().poolWithId(BasicModSetup.ORIGIN_DIMENSION_POOL_ID).orElseThrow();
+        var destinationPool = setup.dimensionPoolConfig.state().poolWithId(BasicModSetup.DESTINATION_DIMENSION_POOL_ID).orElseThrow();
+
+        originPool.setGameMode(GameMode.SPECTATOR);
+        destinationPool.setGameMode(GameMode.CREATIVE);
+
+        setup.instance.transitionHandler.handlePlayerDimensionChange(
+            player,
+            BasicModSetup.ORIGIN_DIMENSION,
+            BasicModSetup.DESTINATION_DIMENSION
+        );
+
+        context.testEntity(
+            player,
+            ServerPlayerEntity::isCreative,
+            "Game mode is creative after first transition"
+        );
+
+        setup.instance.transitionHandler.handlePlayerDimensionChange(
+            player,
+            BasicModSetup.DESTINATION_DIMENSION,
+            BasicModSetup.ORIGIN_DIMENSION
+        );
+
+        context.testEntity(
+            player,
+            ServerPlayerEntity::isSpectator,
+            "Game mode is spectator after return transition"
+        );
+
+        context.complete();
+    }
+}
---
+++ src/gametest/java/net/thomilist/dimensionalinventories/gametest/StatusEffectModuleTest.java
@@ -0,0 +1,56 @@
+package net.thomilist.dimensionalinventories.gametest;
+
+import net.fabricmc.fabric.api.entity.FakePlayer;
+import net.fabricmc.fabric.api.gametest.v1.FabricGameTest;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.registry.Registries;
+import net.minecraft.test.GameTest;
+import net.minecraft.test.TestContext;
+import net.thomilist.dimensionalinventories.gametest.util.BasicModSetup;
+
+public class StatusEffectModuleTest
+{
+    // Status effects should be swapped on dimension pool transition.
+    // Test with all registered status effects
+    @GameTest(templateName = FabricGameTest.EMPTY_STRUCTURE)
+    public void transitionSwapsStatusEffects(TestContext context)
+    {
+        var setup = new BasicModSetup();
+
+        // Using a Fabric API FakePlayer instead of a DummyServerPlayerEntity,
+        // since a server connection seems to be required to change status effects
+        var player = FakePlayer.get(context.getWorld());
+
+        for (var effect : Registries.STATUS_EFFECT)
+        {
+            var effectEntry = Registries.STATUS_EFFECT.getEntry(effect);
+            var effectInstance = new StatusEffectInstance(effectEntry);
+            DimensionalInventoriesGameTest.LOGGER.debug(
+                "transitionSwapsStatusEffects: {}", effect.getName().getString());
+
+            player.addStatusEffect(effectInstance);
+
+            setup.instance.transitionHandler.handlePlayerDimensionChange(
+                player,
+                BasicModSetup.ORIGIN_DIMENSION,
+                BasicModSetup.DESTINATION_DIMENSION
+            );
+
+            context.assertTrue(player.getStatusEffects().isEmpty(),
+                "Player has no status effects after transition");
+
+            setup.instance.transitionHandler.handlePlayerDimensionChange(
+                player,
+                BasicModSetup.DESTINATION_DIMENSION,
+                BasicModSetup.ORIGIN_DIMENSION
+            );
+
+            context.assertTrue(player.hasStatusEffect(effectEntry),
+                "Player regained status effect after return transition");
+
+            player.clearStatusEffects();
+        }
+
+        context.complete();
+    }
+}
