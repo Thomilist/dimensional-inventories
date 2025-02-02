b09ee4de69970b103362873279cc5beeb7936666
Thomas Emil Jensen <develop@thomilist.net>
2025-02-02 15:16:43 +0100

    Added assertion helpers to better encapsulate assertion boilerplate
    — Actual test code should now be less cluttered
    — Differences across Minecraft versions can be contained in helper implementations

---
+++ src/gametest/java/net/thomilist/dimensionalinventories/gametest/util/assertion/ItemStackAsserter.java
@@ -0,0 +1,66 @@
+package net.thomilist.dimensionalinventories.gametest.util.assertion;
+
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.test.TestContext;
+
+public class ItemStackAsserter
+{
+    private final TestContext context;
+    private final Registry<Enchantment> enchantmentRegistry;
+
+    public ItemStackAsserter( final TestContext context )
+    {
+        this.context = context;
+        this.enchantmentRegistry = context.getWorld().getRegistryManager().getOrThrow( RegistryKeys.ENCHANTMENT );
+    }
+
+    public void assertItemType( final ItemStack itemStack, final Item expectedItem )
+    {
+        this.context.assertTrue(
+            itemStack.isOf( expectedItem ),
+            "Expected item type %s; found %s".formatted( expectedItem, itemStack.getItem() )
+        );
+    }
+
+    public void assertCount( final ItemStack itemStack, final int expectedCount )
+    {
+        this.context.assertEquals( itemStack.getCount(), expectedCount, "%s count".formatted( itemStack.getItem() ) );
+    }
+
+    public void assertDamage( final ItemStack itemStack, final int expectedDamage )
+    {
+        this.context.assertEquals(
+            itemStack.getDamage(),
+            expectedDamage,
+            "%s damage".formatted( itemStack.getItem() )
+        );
+    }
+
+    public void assertName( final ItemStack itemStack, final String expectedName )
+    {
+        this.context.assertEquals(
+            itemStack.getName(),
+            expectedName,
+            "%s custom item name".formatted( itemStack.getItem() )
+        );
+    }
+
+    public void assertEnchantment( final ItemStack itemStack,
+                                   final RegistryKey<Enchantment> expectedEnchantment,
+                                   final int expectedEnchantmentLevel )
+    {
+        this.context.assertEquals(
+            EnchantmentHelper
+                .getEnchantments( itemStack )
+                .getLevel( this.enchantmentRegistry.getOrThrow( expectedEnchantment ) ),
+            expectedEnchantmentLevel,
+            "%s level".formatted( expectedEnchantment )
+        );
+    }
+}
