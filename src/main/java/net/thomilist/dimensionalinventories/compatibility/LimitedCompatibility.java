099df80fadad7ac8f9c0ef2eacb149ab9e3f903d
Thomas Emil Jensen <develop@thomilist.net>
2025-02-07 17:39:41 +0100

    Included javadocs in builds
    — Also fixed minor errors in the few doc comments currently present (unescaped HTML; summaries getting cut off by the period in "e.g.")

--- src/main/java/net/thomilist/dimensionalinventories/compatibility/LimitedCompatibility.java
+++ src/main/java/net/thomilist/dimensionalinventories/compatibility/LimitedCompatibility.java
@@ -6,23 +6,23 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Indicates that a class, interface or method is not compatible with all versions of e.g. Java or Minecraft that the
- * project as a whole supports.
+ * Indicates that a class, interface or method is not compatible with all versions of Java or Minecraft that the project
+ * as a whole supports.
  */
 @Target( { ElementType.TYPE, ElementType.METHOD } )
 @Retention( RetentionPolicy.CLASS )
 public @interface LimitedCompatibility
 {
     /**
-     * The target that the version range applies to, e.g. "Java" or "Minecraft".
+     * The target that the version range applies to, either "Java" or "Minecraft".
      *
      * @return the target that the version range applies to
      */
     String target();
 
     /**
-     * The range of versions that the annotated class, interface or method is compatible with, e.g. ">=17" or ">=1.20 .1
-     * <1.21"
+     * The range of versions that the annotated class, interface or method is compatible with, such as {@code ">=17"} or
+     * {@code ">=1.20.1 <1.21"}
      *
      * @return the range of compatible versions
      */
